4d888336b2c3cc79b7c248ef6ec91564d041eefd
Soham De Roy <sohamderoy@ss3.local>
2023-07-26 01:35:55 +0530

    initial commit

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World, Site under constructions</h1>
+      <h1>Hello World, Blog Site under constructions</h1>
     </div>
   );
 }
